81be8482946173ba6f5dceb154085e8fc2e5ab30
dofuusdelinquest <164105802+dofuusdelinquest@users.noreply.github.com>
2024-03-20 08:36:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
+var games = [251570, 240, 374320, 570940, 4000, 232090, 2529820, 2194530, 1120480];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
